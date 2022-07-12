0d6110337dd7d5950d106d040f698cf252527b14
João Victor <jaodisc@gmail.com>
2022-07-12 20:40:35 -0300

    Add files via upload

---
+++ oders.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Pendente',
+        shipping: 'Pendente'
+    },
+    {
+        productName: 'LARVENDER KF102 Drone',
+        productNumber: '36378',
+        paymentStatus: 'Reembolsado',
+        shipping: 'Recusado'
+    },
+    {
+        productName: 'Rkuo F11 Pro Drone',
+        productNumber: '49347',
+        paymentStatus: 'Pendente',
+        shipping: 'Pendente'
+    },
+    {
+        productName: 'Drone with Camera Drone',
+        productNumber: '96996',
+        paymentStatus: 'Pago',
+        shipping: 'Entregue'
+    },
+    {
+        productName: 'GPS 4k Drone',
+        productNumber: '22821',
+        paymentStatus: 'Pago',
+        shipping: 'Entregue'
+    },
+    {
+        productName: 'DJI Air 25',
+        productNumber: '81475',
+        paymentStatus: 'Pendente',
+        shipping: 'Pendente'
+    },
+    {
+        productName: 'Lozenge Drone',
+        productNumber: '00482',
+        paymentStatus: 'Pago',
+        shipping: 'Entregue'
+    },
+]
